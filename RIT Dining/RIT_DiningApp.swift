7534ad69fe088e8f3da8f9966fefdea9a347f06c
NinjaCheetah <58050615+NinjaCheetah@users.noreply.github.com>
2025-08-31 10:38:38 -0400

    Initial Commit

---
+++ RIT Dining/RIT_DiningApp.swift
@@ -0,0 +1,17 @@
+//
+//  RIT_DiningApp.swift
+//  RIT Dining
+//
+//  Created by Campbell Bagley on 8/31/25.
+//
+
+import SwiftUI
+
+@main
+struct RIT_DiningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
